8d03abe01dd6a92436f5745de780e9591f307cd1
Karim Aloulou <karim.aloulou@esprit.tn>
2025-01-19 18:21:49 +0100

    final push

--- app/models.py
+++ app/models.py
@@ -1,15 +1,26 @@
-# Modèle de la Base de Données avec SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-from app import db
+db = SQLAlchemy()
 
 class Attack(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     attack_type = db.Column(db.String(50), nullable=False)
     target_ip = db.Column(db.String(100), nullable=False)
-    result = db.Column(db.String(200), nullable= False)
-    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+    result = db.Column(db.String(500), nullable=True)
     
-def __init__(self, attack_type, target_ip, result):
-    self.attack_type = attack_type
-    self.target_ip = target_ip
-    self.result = result
+    # Colonnes spécifiques à l'attaque DoS
+    successful_responses = db.Column(db.Integer, default=0)
+    error_responses = db.Column(db.Integer, default=0)
+    timeouts = db.Column(db.Integer, default=0)
+    errors = db.Column(db.Integer, default=0)
+    average_latency = db.Column(db.Float, default=0.0)
+
+    def __init__(self, attack_type, target_ip, result, successful_responses=0, error_responses=0, timeouts=0, errors=0, average_latency=0.0):
+        self.attack_type = attack_type
+        self.target_ip = target_ip
+        self.result = result
+        self.successful_responses = successful_responses
+        self.error_responses = error_responses
+        self.timeouts = timeouts
+        self.errors = errors
+        self.average_latency = average_latency
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-# Ce fichier dans lequel on ecrira les routes pour gérer les attaques et générer les rapports 
 import asyncio
+import logging
 from flask import Blueprint, request, jsonify, render_template, send_file, current_app
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
@@ -9,90 +9,130 @@ import time
 import os
 from app.attacks import brute_force_attack
 from app.attacks import sql_injection_attack
+from app.attacks import dos
+from app.attacks import dbSchema
+
+# Configurer le logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
+    logger.info("Accès à la page d'accueil.")
     return render_template('index.html')
 
 
-@main.route('/attack', methods = ['POST'])
+@main.route('/attack', methods=['POST'])
 def attack():
-    attack_type = request.json['attackType']
-    target_ip = request.json['targetIP']
+    logger.info("Requête reçue pour l'attaque.")
+    attack_type = request.json.get('attackType', '')
+    target_ip = request.json.get('targetIP', '')
     results = {}
+
+    logger.info(f"Type d'attaque : {attack_type}, IP cible : {target_ip}")
     
-    # simulation du temps d'exécution d'une attaque a 3secondes
+    # Simulation du temps d'exécution d'une attaque à 3 secondes
     time.sleep(3)
     
-    # implementation de la logique des attaques 
-    if attack_type == "bruteForce":
-       result_message = brute_force_attack(target_ip)
-    elif attack_type == "dos":
-        result_message = f"Attaque DoS sur {target_ip} exécutée."
-    elif attack_type == "sqlInjection":
-        # result_message = sql_injection_attack(target_ip)
-        result_message = asyncio.run(sql_injection_attack(target_ip))
-    elif attack_type == "xss":
-        result_message = f"Cross-Site Scripting sur {target_ip} simulé."
-    else:
-        result_message = "Type d'attaque non reconnu."
-    
-    # enregistrement dans la base de données 
-    attack_record = Attack(attack_type=attack_type, target_ip=target_ip, result=result_message)
-    db.create_all()
-    db.session.add(attack_record)
-    db.session.commit()
-    
-    results['Messages'] = result_message
+    # Implémentation de la logique des attaques 
+    try:
+        if attack_type == "bruteForce":
+            logger.info("Démarrage de l'attaque brute force.")
+            result_message = brute_force_attack(target_ip)
+
+        elif attack_type == "dos":
+            logger.info("Démarrage de l'attaque DoS.")
+            request_count = 100
+            interval = 100
+            duration = 10
+            thread_count = 10
+            result_message = asyncio.run(dos.dos_attack(target_ip, request_count=request_count, interval=interval, duration=duration, thread_count=thread_count))
+            dos.generate_report(dos.results, result_message)
+
+        elif attack_type == "sqlInjection":
+            logger.info("Démarrage de l'attaque SQL Injection.")
+            result_message = asyncio.run(sql_injection_attack(target_ip))
+
+        elif attack_type == "xss":
+            logger.info("Simulation d'une attaque XSS.")
+            result_message = f"Cross-Site Scripting sur {target_ip} simulé."
+
+        elif attack_type == 'dbSchema':
+            logger.info("Démarrage de l'attaque de mapping de la base de données.")
+            param_list = [
+                "id", "name", "user", "product", "category", "action", "page", "search", "query", "filter",
+                "category_id", "item", "session", "order", "price", "email", "username", "password", "key", "sort"
+            ]
+            result_message = dbSchema.test_parameters(f"{target_ip}", param_list)
+            report_dir = os.path.join(current_app.root_path, 'static', 'reports')
+            if not os.path.exists(report_dir):
+                os.makedirs(report_dir)
+            pdf_path = os.path.join(report_dir, 'dbschema_test_report.pdf')
+            dbSchema.generate_dbschema_report(result_message, pdf_path)
+            logger.info(f"Rapport PDF dbSchema disponible à : {pdf_path}")
+        else:
+            logger.warning(f"Type d'attaque non reconnu : {attack_type}")
+            result_message = "Type d'attaque non reconnu."
+
+        # Enregistrement dans la base de données 
+        logger.info("Enregistrement de l'attaque dans la base de données.")
+        attack_record = Attack(attack_type=attack_type, target_ip=target_ip, result=result_message)
+        db.create_all()
+        db.session.add(attack_record)
+        db.session.commit()
+        
+        results['Messages'] = result_message
+        logger.info("Attaque terminée avec succès.")
+
+    except Exception as e:
+        logger.error(f"Erreur lors du traitement de l'attaque : {e}")
+        results['Messages'] = f"Erreur : {e}"
+
     return jsonify(results)
+
 
 @main.route('/attacks', methods=['GET'])
 def get_attacks_pdf():
+    logger.info("Génération du fichier PDF des attaques.")
     attacks = Attack.query.all()
     
-    # Chemin du fichier PDF
-    report_dir = os.path.join(current_app.root_path, 'static', 'reports') 
-    # report_dir = 'static/reports'
+    report_dir = os.path.join(current_app.root_path, 'static', 'reports')
     if not os.path.exists(report_dir):
         os.makedirs(report_dir)
     
     pdf_path = os.path.join(report_dir, 'report.pdf')
 
-    # Génération du fichier PDF
-    c = canvas.Canvas(pdf_path, pagesize=letter)
-    width, height = letter
-    c.setFont("Helvetica", 12)
-    
-    # Titre du rapport
-    c.drawString(100, height - 50, "Attacks Report")
-    
-    # En-tête des colonnes
-    c.drawString(50, height - 80, "ID")
-    c.drawString(100, height - 80, "Type d'Attaque")
-    c.drawString(250, height - 80, "IP Cible")
-    c.drawString(350, height - 80, "Résultat")
-    c.drawString(450, height - 80, "Timestamp")
+    try:
+        c = canvas.Canvas(pdf_path, pagesize=letter)
+        width, height = letter
+        c.setFont("Helvetica", 12)
+        c.drawString(100, height - 50, "Attacks Report")
+        c.drawString(50, height - 80, "ID")
+        c.drawString(100, height - 80, "Type d'Attaque")
+        c.drawString(250, height - 80, "IP Cible")
+        c.drawString(350, height - 80, "Résultat")
+        c.drawString(450, height - 80, "Timestamp")
 
-    y_position = height - 100
-    
-    # Parcourir et écrire les attaques dans le PDF
-    for attact in attacks:
-        if y_position < 50:  # Si on arrive au bas de la page, ajouter une nouvelle page
-            c.showPage()
-            c.setFont("Helvetica", 12)
-            y_position = height - 50
-        
-        c.drawString(50, y_position, str(attact.id))
-        c.drawString(100, y_position,attact.attack_type[:30])
-        c.drawString(250, y_position,attact.target_ip[:40])
-        c.drawString(350, y_position,attact.result[:30])
-        c.drawString(450, y_position, attact.timestamp.strftime("%Y-%m-%d %H:%M:%S"))
+        y_position = height - 100
 
-        y_position -= 20  # Descendre d'une ligne
+        for attack in attacks:
+            if y_position < 50:
+                c.showPage()
+                c.setFont("Helvetica", 12)
+                y_position = height - 50
+            
+            c.drawString(50, y_position, str(attack.id))
+            c.drawString(100, y_position, attack.attack_type[:30])
+            c.drawString(250, y_position, attack.target_ip[:40])
+            c.drawString(350, y_position, attack.result[:30])
+            c.drawString(450, y_position, attack.timestamp.strftime("%Y-%m-%d %H:%M:%S"))
+            y_position -= 20
 
-    c.save()  # Sauvegarder le PDF
-
-    # Envoyer le fichier PDF en tant que pièce jointe
-    return send_file(pdf_path, as_attachment=True)
+        c.save()
+        logger.info("Fichier PDF généré avec succès.")
+        return send_file(pdf_path, as_attachment=True)
+    except Exception as e:
+        logger.error(f"Erreur lors de la génération du fichier PDF : {e}")
+        return jsonify({"error": str(e)}), 500
